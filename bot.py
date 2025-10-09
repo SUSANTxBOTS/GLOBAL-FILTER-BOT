a341b58692f9c0811d6d98574205934b59df3d36
SUSANTxBOTS <susantpc1232@gmail.com>
2025-10-09 11:10:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ TMDB_API_KEY = "371c10909d11f866a3a1786e3a43cd8e"
 TMDB_BASE_URL = "https://api.themoviedb.org/3"
 OWNER_IDS = [8156708830, 7125448912, 987654321, 7968389767, 8085299659]
 BACKUP_CHANNEL = "https://t.me/+zSzt4s9xq_ZmZWZl"
-BOT_CHANNEL = "https://t.me/ThronexCodex'
+BOT_CHANNEL = "https://t.me/ThronexCodex"
 #///////////////////////////////////////////////////////////////////////////////
 try:
     import requests
